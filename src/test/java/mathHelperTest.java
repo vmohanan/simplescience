18a49c6e2e68f7e5e523618872c783ef0e40795c
Vinod <Vinod.Mohanan@gmail.com>
2015-12-30 14:01:41 -0500

    few tests for add functions (able to clean install, but somehow idea doesn't like this test structure, need to look at it later)

---
+++ src/test/java/mathHelperTest.java
@@ -0,0 +1,28 @@
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import com.simplesciences.MathHelper;
+
+public class mathHelperTest {
+    @Test
+    public void AddTest()
+    {
+        MathHelper mc=new MathHelper();
+        assertEquals(mc.add(4,5),9);
+        assertEquals(mc.add(0,5),5);
+        assertEquals(mc.add(0,0),0);
+        assertEquals(mc.add(100,100),200);
+    }
+
+    @Test
+    public void AddNegativeNumbersTest()
+    {
+        MathHelper mc=new MathHelper();
+        assertEquals(mc.add(-4,5),1);
+        assertEquals(mc.add(0,-5),-5);
+        assertEquals(mc.add(-1,-2),-3);
+        assertEquals(mc.add(-1,1),0);
+    }
+}
