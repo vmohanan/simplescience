a7ed45c269cb00677f4eb40ce4d037eea3b66c80
Vinod <Vinod.Mohanan@gmail.com>
2016-01-03 23:55:38 -0500

    sample commit after simple change to trigger build

--- src/test/java/mathHelperTest.java
+++ src/test/java/mathHelperTest.java
@@ -12,7 +12,6 @@ public class mathHelperTest {
         MathHelper mc=new MathHelper();
         assertEquals(mc.add(4,5),9);
         assertEquals(mc.add(0,5),5);
-        assertEquals(mc.add(0,0),0);
         assertEquals(mc.add(100,100),200);
     }
 
@@ -24,5 +23,15 @@ public class mathHelperTest {
         assertEquals(mc.add(0,-5),-5);
         assertEquals(mc.add(-1,-2),-3);
         assertEquals(mc.add(-1,1),0);
+    }
+
+    @Test
+    public void AddNZeroesTest()
+    {
+        MathHelper mc=new MathHelper();
+        assertEquals(mc.add(-4,0),-4);
+        assertEquals(mc.add(0,-15),-15);
+        assertEquals(mc.add(0,-2),-2);
+        assertEquals(mc.add(0,0),0);
     }
 }
